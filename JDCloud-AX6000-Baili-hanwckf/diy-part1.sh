4f601d6b9620a10024e14f0ad6e06da53db11dd6
nywangyu <lbxshu@163.com>
2025-01-23 12:00:50 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
@@ -12,13 +12,11 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git dahuilang https://github.com/shidahuilang/openwrt-package;Immortalwrt' >> feeds.conf.default
+echo 'src-git dahuilang https://github.com/shidahuilang/openwrt-package;Immortalwrt' >> feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 #git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 #git clone -b main https://github.com/ilxp/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
