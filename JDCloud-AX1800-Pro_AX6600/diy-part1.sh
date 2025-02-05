6733cf80a993ce390e0fba9d2c42ea8fb545971d
nywangyu <lbxshu@163.com>
2025-02-05 12:57:53 +0800

    Update diy-part1.sh

--- JDCloud-AX1800-Pro_AX6600/diy-part1.sh
+++ JDCloud-AX1800-Pro_AX6600/diy-part1.sh
@@ -12,8 +12,10 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Add a feed source
 #git clone https://github.com/kenzok8/openwrt-packages package/kenzo
 #git clone https://github.com/kenzok8/small package/small
-echo 'src-git dahuilang https://github.com/shidahuilang/openwrt-package;Lede' >> feeds.conf.default
+#echo 'src-git dahuilang https://github.com/shidahuilang/openwrt-package;Lede' >> feeds.conf.default
