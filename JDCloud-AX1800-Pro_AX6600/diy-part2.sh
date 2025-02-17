f842b3862f76c12b21447d62aa043fc8a427e3eb
nywangyu <lbxshu@163.com>
2025-02-17 21:55:39 +0800

    Update diy-part2.sh

--- JDCloud-AX1800-Pro_AX6600/diy-part2.sh
+++ JDCloud-AX1800-Pro_AX6600/diy-part2.sh
@@ -10,6 +10,15 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 ##-----------------Add dev core for kenzo OpenClash------------------
