bf81c439515c28aaf7075725483923a6636ce6c0
nywangyu <lbxshu@163.com>
2026-03-20 14:49:55 +0800

    Update diy-part1.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part1.sh
@@ -12,7 +12,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i '$a src-git jell https://github.com/kenzok8/jell' feeds.conf.default && ./scripts/feeds update -a && ./scripts/feeds install -a
+sed -i '$a src-git jell https://github.com/kenzok8/jell' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
